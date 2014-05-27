be30e23293b45cbde3352966dba008f10a0cd600
klehner <klaus.lehner@catalysts.cc>
2014-05-27 14:22:18 +0200

    add some annotations readme

--- src/main/java/net/klu2/spring/boot/swagger/web/Greeting.java
+++ src/main/java/net/klu2/spring/boot/swagger/web/Greeting.java
@@ -1,10 +1,16 @@
 package net.klu2.spring.boot.swagger.web;
 
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
 /**
  * @author Klaus Lehner, Catalysts GmbH
  */
+@ApiModel(value = "Encapsulates one greeting")
 public class Greeting {
+    @ApiModelProperty(required = true, notes = "The number of calls so far")
     private final long id;
+    @ApiModelProperty(required = true, notes = "The response message")
     private final String content;
 
     public Greeting(long id, String content) {
--- src/main/java/net/klu2/spring/boot/swagger/web/GreetingController.java
+++ src/main/java/net/klu2/spring/boot/swagger/web/GreetingController.java
@@ -1,22 +1,22 @@
 package net.klu2.spring.boot.swagger.web;
 
+import com.wordnik.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
+@RestController
+@Api(basePath="greeting", value="Send some greetings", description = "This very friendly controller listens to you and greets you!")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    @ResponseBody
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
